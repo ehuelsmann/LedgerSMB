2c4e11b09d62328fc48076ab0d719f66abce1b64
Erik Huelsmann <ehuels@gmail.com>
2019-06-30 15:32:03 +0200

    * Convert the menu to be a Vue widget

---
+++ UI/js-src/lsmb/MenuItem.js
@@ -0,0 +1,68 @@
+
+export default {
+  template: `
+    <li>
+<div style="display:inline-block; width: 100%" class="dijitTreeRow" v-bind:class="{ dijitTreeRowHover : hover }" @mouseenter="hover = true" @mouseleave="hover = false"  @click="toggle">
+      <a @click="toggle" v-if="!item.module" :aria-expanded="open ? 'true' : 'false'">
+        <span v-if="isFolder" class="dijitInline dijitTreeExpando" v-bind:class="{ dijitTreeExpandoOpened : open, dijitTreeExpandoClosed : !open, dijitTreeNodeHover : hover }"></span>{{item.label}}
+
+      </a>
+      <a @click="follow" v-if="item.module" :href="url">
+        <span class="dijitInline dijitTreeExpando dijitTreeExpandoLeaf"></span>{{item.label}}
+      </a>
+</div>
+    <ul v-if="expanded" class="dijitTreeNodeContainer">
+      <menu-item
+        v-for="child,indx in children"
+        :item="child"
+        :key="indx"
+        :menudata="menudata"
+        ></menu-item>
+    </ul>
+    </li>
+  `,
+  props: [
+    'item',
+    'menudata',
+  ],
+  name: 'menu-item',
+  data() {
+    return {
+        open: false,
+        hover: false,
+    }
+  },
+  computed: {
+    children() {
+      const childr = this.menudata.filter(child => {
+        return this.item.id === child.parent;
+      });
+      return childr;
+    },
+    isFolder() {
+      return this.children.length > 0;
+    },
+    expanded() {
+      if (this.children) {
+        return this.open;
+      }
+      return false;
+    },
+    url() {
+      return this.item.module + '?'
+        + this.item.args.join('&');
+    },
+  },
+  methods: {
+    toggle(event) {
+        this.open = !this.open;
+        if (event) {
+            event.stopPropagation();
+        }
+    },
+    follow(event) {
+      console.log(this.url);
+      event.preventDefault();
+    },
+  },
+}
---
+++ UI/js-src/lsmb/MenuTree.js
@@ -0,0 +1,42 @@
+
+import MenuItem from './MenuItem.js'
+
+export default {
+  template: `
+    <div class="dijitTreeContainer">
+      <ul class="menu">
+      <menu-item
+        v-for="child,indx in children"
+        :key="indx"
+        :item="child"
+        :menudata="menuitemData"
+     ></menu-item>
+     </ul>
+    </div>
+  `,
+  props: [
+    'source',
+  ],
+  data() {
+    return {
+      menuitemData: [],
+    }
+  },
+  components: {
+    MenuItem,
+  },
+  computed: {
+    children() {
+      const childr = this.menuitemData.filter(child => {
+        return child.parent === 0;
+      });
+      return childr;
+
+    }
+  },
+  async created() {
+    const response = await axios.get(this.source);
+    this.menuitemData = response.data;
+  },
+
+}
